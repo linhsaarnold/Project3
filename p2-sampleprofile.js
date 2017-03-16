89e9516ff828c95389df9bdf0b43a099e9ec024a
linhsaarnold <linhsa.arnold@gmail.com>
2017-03-15 18:18:14 -0700

    inital commit, linhsa

---
+++ p2-sampleprofile.js
@@ -0,0 +1,15 @@
+function navbarsearch() {
+    alert("I'm sorry, we can't find what you're looking for.");
+}
+
+function moreinfo() {
+    alert("No Info Available");
+}
+
+var countA = 0;
+function addinterestA() {
+    countA++;
+    document.getElementById("IntA").innerHTML = countA;
+}
+
+
