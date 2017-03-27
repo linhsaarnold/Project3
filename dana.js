4c06515a0c0f5ed374023b9fe2f24577497f47d1
linhsaarnold <linhsa.arnold@gmail.com>
2017-03-27 12:43:38 -0700

    almost finished my prod profile

---
+++ dana.js
@@ -0,0 +1,27 @@
+function navbarsearch() {
+    alert("I'm sorry, we can't find what you're looking for.");
+}
+
+function moreinfo() {
+    alert("No Info Available");
+}
+
+var countA = 0;
+function addinterestA() {
+    countA++;
+    document.getElementById("IntA").innerHTML = countA;
+}
+
+function editfaculty() {
+    txt1 = document.getElementById("changefaculty").value;
+    document.getElementById("faculty").innerHTML = txt1;
+}
+
+function edityear() {
+
+}
+
+function edithometown() {
+
+}
+
--- p2-clothing.js
+++ p2-clothing.js
@@ -12,4 +12,50 @@ function addinterestA() {
     document.getElementById("IntA").innerHTML = countA;
 }
 
+var countB = 0;
+function addinterestB() {
+    countB++;
+    document.getElementById("IntB").innerHTML = countB;
+}
 
+var countC = 0;
+function addinterestC() {
+    countC++;
+    document.getElementById("IntC").innerHTML = countC;
+}
+
+var countD = 0;
+function addinterestD() {
+    countD++;
+    document.getElementById("IntD").innerHTML = countD;
+}
+
+var countE = 0;
+function addinterestE() {
+    countE++;
+    document.getElementById("IntE").innerHTML = countE;
+}
+
+var countF = 0;
+function addinterestF() {
+    countF++;
+    document.getElementById("IntF").innerHTML = countF;
+}
+
+var countG = 0;
+function addinterestG() {
+    countG++;
+    document.getElementById("IntG").innerHTML = countG;
+}
+
+var countH = 0;
+function addinterestH() {
+    countH++;
+    document.getElementById("IntH").innerHTML = countH;
+}
+
+var countI = 0;
+function addinterestI() {
+    countI++;
+    document.getElementById("IntI").innerHTML = countI;
+}
