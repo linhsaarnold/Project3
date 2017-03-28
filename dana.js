f8e789af176d67ad96f4591c392d841491f8f0cb
linhsaarnold <linhsa.arnold@gmail.com>
2017-03-27 22:46:07 -0700

    fixed the js to edit faculty, hometown, and year on the myprofile page

--- dana.js
+++ dana.js
@@ -12,16 +12,73 @@ function addinterestA() {
     document.getElementById("IntA").innerHTML = countA;
 }
 
+var countB = 0;
+function addinterestB() {
+    countB++;
+    document.getElementById("IntB").innerHTML = countB;
+}
+
+var countC = 0;
+function addinterestC() {
+    countC++;
+    document.getElementById("IntC").innerHTML = countC;
+}
+
+var countD = 0;
+function addinterestD() {
+    countD++;
+    document.getElementById("IntD").innerHTML = countD;
+}
+
+var countE = 0;
+function addinterestE() {
+    countE++;
+    document.getElementById("IntE").innerHTML = countE;
+}
+
+var countF = 0;
+function addinterestF() {
+    countF++;
+    document.getElementById("IntF").innerHTML = countF;
+}
+
+var countG = 0;
+function addinterestG() {
+    countG++;
+    document.getElementById("IntG").innerHTML = countG;
+}
+
+var countH = 0;
+function addinterestH() {
+    countH++;
+    document.getElementById("IntH").innerHTML = countH;
+}
+
+var countI = 0;
+function addinterestI() {
+    countI++;
+    document.getElementById("IntI").innerHTML = countI;
+}
+
 function editfaculty() {
-    txt1 = document.getElementById("changefaculty").value;
-    document.getElementById("faculty").innerHTML = txt1;
+    txt1 = document.getElementById("inputfaculty").value;
+    document.getElementById("printfaculty").innerHTML = txt1;
 }
 
 function edityear() {
-
+    txt2 = document.getElementById("inputyear").value;
+    document.getElementById("printyear").innerHTML = txt2;
 }
 
 function edithometown() {
-
+    txt3 = document.getElementById("inputhometown").value;
+    document.getElementById("printhometown").innerHTML = txt3;
 }
+
+
+
+
+
+
+
 
